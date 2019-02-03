438211e7f9ddd5a3c095cf8e0763f716ea78be6b
gubatron <gubatron@gmail.com>
2019-02-02 20:11:03 -0700

    updates to hmwk2
     - safeGetline (so it reads ignoreWords.txt correctly on macos)
     - quickSort implementation
     - refactor of arraySort to use bubbleSort (commented) or quickSort with a simple comment

--- CSCI2270/Hmwk/hmwk2/wordAnalysis.cpp
+++ CSCI2270/Hmwk/hmwk2/wordAnalysis.cpp
@@ -32,7 +32,7 @@ struct wordItem {
  *      - allocate the array dynamically and copy values from old array to new array
  *      * DON'T use std::vector class - write it yourself*/
 
-void doubleArray(wordItem* &arrayPtr, int &capacity) {
+void doubleArray(wordItem *&arrayPtr, int &capacity) {
     // dynamically allocate an array of size newCapacity
     auto *newArray = new wordItem[capacity * 2];
 
@@ -46,9 +46,41 @@ void doubleArray(wordItem* &arrayPtr, int &capacity) {
     arrayPtr = newArray; // re-reference the de-reference
     capacity *= 2;
 }
-////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-void getStopWords(const char *ignoreWordFileName, std::string ignoreWords[]) {
 
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+std::istream &safeGetline(std::istream &is, std::string &t) {
+    t.clear();
+
+    // The characters in the stream are read one-by-one using a std::streambuf.
+    // That is faster than reading them one-by-one using the std::istream.
+    // Code that uses streambuf this way must be guarded by a sentry object.
+    // The sentry object performs various tasks,
+    // such as thread synchronization and updating the stream state.
+
+    std::istream::sentry se(is, true);
+    std::streambuf *sb = is.rdbuf();
+
+    for (;;) {
+        int c = sb->sbumpc();
+        switch (c) {
+            case '\n':
+                return is;
+            case '\r':
+                if (sb->sgetc() == '\n')
+                    sb->sbumpc();
+                return is;
+            case std::streambuf::traits_type::eof():
+                // Also handle the case when the last line has no line ending
+                if (t.empty())
+                    is.setstate(std::ios::eofbit);
+                return is;
+            default:
+                t += (char) c;
+        }
+    }
+}
+
+void getStopWords(const char *ignoreWordFileName, std::string ignoreWords[]) {
     int element = 0;
     std::string tempword;
     std::ifstream input(ignoreWordFileName);
@@ -59,7 +91,7 @@ void getStopWords(const char *ignoreWordFileName, std::string ignoreWords[]) {
         return;
     }
 
-    while (getline(input, tempword) && element < 50) {
+    while (safeGetline(input, tempword) && element < 50) {
         ignoreWords[element++] = tempword;
     }
 
@@ -93,34 +125,93 @@ void swap(wordItem *a, wordItem *b) {
     *b = temp;
 }
 
-// Bubble sort
-void arraySort(wordItem arr[], int length) {
+void bubbleSort(wordItem arr[], int length) {
     int i, j;
-    for (i = 0; i < length-1; i++)
-        for (j = 0; j < length-i-1; j++)
-            if (arr[j].count < arr[j+1].count)
-                swap(&arr[j], &arr[j+1]);
+    for (i = 0; i < length - 1; i++)
+        for (j = 0; j < length - i - 1; j++)
+            if (arr[j].count < arr[j + 1].count)
+                swap(&arr[j], &arr[j + 1]);
+}
+
+void quickSort(wordItem arr[], int length, bool reverse) {
+    if (length <= 1) {
+        return;
+    }
+    const int pivot_count = arr[length / 2].count;
+    int smaller_length = 0;
+    int bigger_length = 0;
+    int equal_length = 0;
+
+    for (int i = 0; i < length; i++) {
+        if (!reverse) {
+            if (arr[i].count < pivot_count) smaller_length++;
+            if (arr[i].count > pivot_count) bigger_length++;
+        } else {
+            if (arr[i].count > pivot_count) smaller_length++;
+            if (arr[i].count < pivot_count) bigger_length++;
+        }
+        if (arr[i].count == pivot_count) equal_length++;
+    }
+
+    auto smaller = new wordItem[smaller_length];
+    auto bigger = new wordItem[bigger_length];
+    auto equal = new wordItem[equal_length];
+
+    int smaller_i = 0;
+    int bigger_i = 0;
+    int equal_i = 0;
+
+    // comparisons, this part should be pluggable to make this generic
+    for (int i = 0; i < length; i++) {
+        if (!reverse) {
+            if (arr[i].count < pivot_count) smaller[smaller_i++] = arr[i];
+            if (arr[i].count > pivot_count) bigger[bigger_i++] = arr[i];
+        } else {
+            if (arr[i].count > pivot_count) smaller[smaller_i++] = arr[i];
+            if (arr[i].count < pivot_count) bigger[bigger_i++] = arr[i];
+        }
+        if (arr[i].count == pivot_count) equal[equal_i++] = arr[i];
+    }
+
+    quickSort(smaller, smaller_length, reverse);
+    quickSort(bigger, bigger_length, reverse);
+
+    int j = 0;
+    // now overwrite the original array with:
+    // smaller + equal + bigger
+
+    for (int i = 0; i < smaller_length; i++) arr[j++] = smaller[i];
+    for (int i = 0; i < equal_length; i++) arr[j++] = equal[i];
+    for (int i = 0; i < bigger_length; i++) arr[j++] = bigger[i];
+
+}
+
+void arraySort(wordItem arr[], int length) {
+    //bubbleSort(arr, length);
+    quickSort(arr, length, true);
 }
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-float probOfOcc(wordItem item, int totalWords){
-    return (float) item.count/totalWords;
+float probOfOcc(wordItem item, int totalWords) {
+    return (float) item.count / totalWords;
 }
 
 void printTopN(wordItem uniqueWords[], int topN, int totalNumWords) {
     // assume array is already sorted
-    for(int i = 0; i < topN; i++){
-        std::cout << std::fixed << std::setprecision(4) << probOfOcc(uniqueWords[i], totalNumWords) << " - " << uniqueWords[i].word.c_str() << std::endl;
+    for (int i = 0; i < topN; i++) {
+        std::cout << std::fixed << std::setprecision(4) << probOfOcc(uniqueWords[i], totalNumWords) << " - "
+                  << uniqueWords[i].word.c_str() << std::endl;
     }
 }
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-int findWord(const std::string &word, wordItem* items, int size){
-    for (int i = 0; i < size; i++){
+int findWord(const std::string &word, wordItem *items, int size) {
+    for (int i = 0; i < size; i++) {
         if (word == items[i].word) return i;
     }
     return -1; // found nothing
 }
+
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 int main(int argc, char *argv[]) {
     if (argc != 4) {
@@ -136,7 +227,6 @@ int main(int argc, char *argv[]) {
     std::string ignoreWords[50];
     getStopWords(ignorewords_filename, ignoreWords);
 
-
     // open file with good words (index 2 argument)
     char *words_filename = argv[2];
     std::ifstream wordsfile(words_filename);
@@ -147,13 +237,16 @@ int main(int argc, char *argv[]) {
     auto *items = new wordItem[capacity];
 
     if (!wordsfile.is_open()) {
-        std::cout << "Failed to open " << words_filename<< std::endl;
+        std::cout << "Failed to open " << words_filename << std::endl;
     }
+
     int size = 0; // used words inside the array
     int doublings = 0;
     std::string tempword;
     while (getline(wordsfile, tempword, ' ')) {
-        if (isStopWord(tempword, ignoreWords)) continue;
+        if (isStopWord(tempword, ignoreWords)) {
+            continue;
+        }
         int wordIndex = findWord(tempword, items, size);
         // if not in the array, add wordItem into array and increment size
         if (wordIndex == -1) {
@@ -179,9 +272,9 @@ int main(int argc, char *argv[]) {
     std::cout << "#" << std::endl;
     std::cout << "Total non-common words: " << getTotalNumberNonStopWords(items, size) << std::endl;
     std::cout << "#" << std::endl;
-    std::cout << "Probabilities of top " <<  topN << " most frequent words " << std::endl;
+    std::cout << "Probabilities of top " << topN << " most frequent words " << std::endl;
     std::cout << "---------------------------------------" << std::endl;
     printTopN(items, topN, getTotalNumberNonStopWords(items, size));
-    delete []items;
+    delete[]items;
 }
 
