50b86ef533adef48ffbeb898389967f917e5e2f2
nicoledraw <nile8832@colorado.edu>
2019-02-09 12:37:04 -0700

    annotated functions and worked on deleteCountry and searchNetwork

--- CSCI_2270/Hmwk/hmwk3/CountryNetwork.cpp
+++ CSCI_2270/Hmwk/hmwk3/CountryNetwork.cpp
@@ -27,10 +27,8 @@ CountryNetwork::CountryNetwork() {
  * @return true if empty; else false
  */
 bool CountryNetwork::isEmpty() {
-    return head == nullptr;
-
-    //    if (head == nullptr) return true;
-    //    return false;
+    if (head == nullptr) return true;
+    return false;
     // todo is this the same as saying  return head == nullptr; ?
 }
 /****************************************************************/
@@ -44,20 +42,18 @@ bool CountryNetwork::isEmpty() {
  * @return none
  */
 void CountryNetwork::insertCountry(Country *previous, Country countryName) {
-//    Country *newnode = countryName;
-    auto prev = previous; // apply search function??
-    auto * value = new Country(std::move(countryName));
+    Country *head = nullptr;
+    auto prev = /*searchNetwork(countryName);*/previous; // apply search function??
+    auto *value = new Country(std::move(countryName));
 
     if (prev == nullptr) {
-        value->next =  head;
+        value->next = head;
         head = value;
         if (prev == nullptr) prev = head;
-    }
-    else if (prev->next == nullptr) {
+    } else if (prev->next == nullptr) {
         prev->next = value;
         // tail = value;
-    }
-    else {
+    } else {
         value->next = prev->next;
         prev->next = value;
     }
@@ -70,10 +66,51 @@ void CountryNetwork::insertCountry(Country *previous, Country countryName) {
  * @return none
  */
 void CountryNetwork::deleteCountry(string countryName) {
-    //searchNetwork(countryName); // find the node with name countryName
-    // then delete it.
-    // If there is no node with name countryName, print "Country does not exist."
+    // TRAVERSE list to find the node with name countryName then delete it
+    Country *head;
+    Country *curr = head;
 
+    // if list is empty
+    if (head == nullptr) return;
+
+    if (curr == nullptr) {
+        cout << "Country does not exist" << endl;
+        return;
+    }
+    while (curr != nullptr) {
+        if (curr->name == countryName) {
+            curr = curr->next;
+            // if at the head
+            if (head->name == countryName) {
+                Country *temp = head;
+                head = head->next;
+                delete temp;
+            }
+                // either tail or middle
+            else {
+                Country *prev = head;
+                curr = prev->next;
+                bool isFound = false;
+                while (curr != nullptr && !isFound) {
+                    if (curr->name == countryName) {
+                        if (curr->next == nullptr) {
+                            // tail node
+                            prev->next = nullptr;
+                            // tail = prev;
+                        } else {
+                            prev->next = curr->next;
+                        }
+                        delete curr;
+                        isFound = true;
+                    } else {
+                        prev = curr;
+                        curr = curr->next;
+                        delete prev; // todo where do I delete? time limit exceeded ERROR
+                    }
+                }
+            }
+        }
+    }
 }
 /****************************************************************/
 /****************************************************************/
@@ -83,6 +120,16 @@ void CountryNetwork::deleteCountry(string countryName) {
  * @return none
  */
 void CountryNetwork::loadDefaultSetup() {
+    // delete whatever is in the linked list using deleteEntireNetwork
+    string name; // why not Country name??
+    deleteCountry(name);
+    // add the countries to network using insertCountry
+//    insertCountry("United States"); // what do I put as previous???
+//    insertCountry("Canada");
+//    insertCountry("Brazil");
+//    insertCountry("India");
+//    insertCountry("China");
+//    insertCountry("Australia")
 
 }
 /****************************************************************/
@@ -94,10 +141,15 @@ void CountryNetwork::loadDefaultSetup() {
  * @see insertCountry, deletecountry
  */
 Country *CountryNetwork::searchNetwork(string countryName) {
-//    Return a pointer to the node with name countryName. If countryName cannot be
-//    found, return NULL
+    Country *curr = head;
+    while (curr != nullptr) {
+        if (curr->name == countryName) return curr;
+        curr = curr->next;
+        return curr;
+    }
+    return nullptr;
 }
-/*********************** -*****************************************/
+/****************************************************************/
 /****************************************************************/
 /*
  * Purpose: deletes all countries in the network starting at the head country.
@@ -105,6 +157,20 @@ Country *CountryNetwork::searchNetwork(string countryName) {
  * @return none
  */
 void CountryNetwork::deleteEntireNetwork() {
+    // if list is empty, do nothing and return
+    // else, delete every node in linked list; set head to NULL
+    // print name of each node as you delete it
+    // cout << "deleting: " << node->name << endl;
+    // after entire linked list is delete, print:
+    // cout << "Deleted network" << endl;
+}
+/****************************************************************/
+/****************************************************************/
+/*
+ * Purpose: reverse the entire network t
+ * @param ptr head of list
+ */
+void CountryNetwork::reverseEntireNetwork() {
 
 }
 /****************************************************************/
@@ -118,7 +184,14 @@ void CountryNetwork::deleteEntireNetwork() {
  * @return none
  */
 void CountryNetwork::transmitMsg(string receiver, string message) {
+    // traverse list from head to node with name receiver
+    // for each node in this path (including head) set node's message to msg
+    // and increment node's numberMessages field.
+    // at each node report the message received and the number of messages received
+    // cout << node->name << " [# messages received: " <<
+    // node->numberMessages << "] received: " << node->message << endl;
 
+    // if empty, print "Empty list" and exit function
 }
 /****************************************************************/
 /****************************************************************/
@@ -127,14 +200,7 @@ void CountryNetwork::transmitMsg(string receiver, string message) {
  * @param ptr head of list
  */
 void CountryNetwork::printPath() {
-
-}
-/****************************************************************/
-/****************************************************************/
-/*
- * Purpose: reverse the entire network t
- * @param ptr head of list
- */
-void CountryNetwork::reverseEntireNetwork() {
-
+    // if network is empty, print "nothing in path"
+    cout << "== CURRENT PATH ==" << endl;
+    cout << "===" << endl;
 }
