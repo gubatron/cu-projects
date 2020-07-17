960775645c60047ac9cf3044a5de265f4b2afa61
Nicole Leon <nicole.leon.ny@gmail.com>
2020-07-16 20:45:14 -0600

    reads all data into array first, then calculates averages for insertion (array copy for now), then writes averages to disk

--- CSCI_2270_2020/homeworks/final_project/arrdriver.cpp
+++ CSCI_2270_2020/homeworks/final_project/arrdriver.cpp
@@ -1,24 +1,34 @@
-#include "dll.hpp"
+// #include "dll.hpp"
 #include <iostream>
 #include <fstream>
 #include <sstream>
 #include <string>
 
-#include <ctime> // for clock()
-// Learned how to use clock() with:
-// http://www.cplusplus.com/reference/ctime/clock/
-
 #include <cstdlib> // for rand() to generate pseudo-random values in search[100]
 // http://www.cplusplus.com/reference/cstdlib/rand/
 
+#include <sys/timeb.h>
 using namespace std;
 
+// simple get time in milliseconds in C http://www.cplusplus.com/forum/general/43203/#msg234281
+long getMilliCount(){
+	timeb tb;
+	ftime(&tb);
+	int nCount = tb.millitm + (tb.time & 0xfffff) * 1000;
+	return nCount;
+}
+
+int random0to99() {
+    return rand() % 100;
+}
+
 // Command Line Example: ./medTracker dll.cpp dlldriver.cpp dataSetA.csv
-main(int argc, char *argv[])
+int main(int argc, char *argv[])
 {
-    int testData[10000];
-    float insert[100];
-    float search[100];
+    int testData[10000]; // we'll first read all the data from the csv and put it in memory, to not interrupt operations with disk I/O later
+    int insertData[100]; // we'll measure 100 times, how long it takes to copy 100 elements here.
+    float insert_avg_times[100];
+    float search_avg_times[100];
 
     // char *dataSetFileName = argv[1];
     ifstream dataSetFile("dataSetA.csv");
@@ -33,12 +43,19 @@ main(int argc, char *argv[])
     int insert_index = 0;
     int search_index = 0;
 
-    auto time = clock();
-    float duration;
+    long start_time;
 
     // read file and insert into testData[]
+    int i=0;
     while (!dataSetFile.eof() && getline(dataSetFile, tempID, ','))
     {
+        testData[i++] = stoi(tempID); // populate testData[1000] 100 numbers at a time
+    }
+    dataSetFile.close();
+    // close file, no more disk I/O
+
+    i=0;
+    while (i < 10000) {
         //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
         // INSERT
         // How long does it take to insert 100 elements into testData[]?
@@ -49,10 +66,14 @@ main(int argc, char *argv[])
 
         if (num_read_items == 0)
         {
-            time; // start the clock
+            start_time = getMilliCount(); // start the clock
+            //  cout << "start_time= " << start_time << endl;
         }
 
-        testData[num_read_items] = stoi(tempID); // populate testData[1000] 100 numbers at a time
+        // INSERT IN ARRAY
+        insertData[num_read_items] = testData[i];
+
+        // SEARCH 
 
         // print to make sure I'm putting the right values inside of testData[].
         // cout << "index: " << num_read_items << "\t tempID: " << tempID << " == " << testData[num_read_items] << endl; // YES!
@@ -63,24 +84,30 @@ main(int argc, char *argv[])
         // 3. Reset the item counter
         if (num_read_items == 99)
         {
-            time = clock() - time;                   // stop the clock
-            duration = time / (float)CLOCKS_PER_SEC; // calculate how long it took to run the function.
+            long end_time = getMilliCount();
+            long duration = end_time - start_time;// calculate how long it took to run the function.
+            // cout << "end_time=" << end_time << endl;
+            // cout << "duration=" << duration << endl << endl;
 
             float avg = duration / 100; // average insert time
-            insert[insert_index] = avg; // put the average in insert[]
+            insert_avg_times[insert_index] = avg; // put the average in insert[]
 
-            // cout << "At INSERT array index 100 * [ " << insert_index << " ] the function took [ " << duration << " ] seconds to INSERT \t";
-            // cout << "The AVERAGE INSERT TIME at INSERT index 100 * [ " << insert_index << " ] is [ " << insert[insert_index] << " ] seconds" << endl;
-
-            // TODO: PRINT AVERAGES INTO 'array_insert_avg.csv' FILE
-            dataResultsInsertFile << insert[insert_index] << ",";
+            cout << "insert[ " << insert_index << " ] Duration [ " << duration << " ] Avg [" << avg <<"]" << endl;
 
             insert_index++;      // go to the next index in my insert array for next time
             num_read_items = -1; // reset item counter so we can start the clock again
         }
         num_read_items++; // brings it back to 0 to continue reading (be able to enter the if statement above)
-
+        i++;
     } // while() insert
+
+    // Now write averages insert times to file
+    for (auto average : insert_avg_times) {
+        dataResultsInsertFile << average << endl;
+    }
+
+    dataResultsInsertFile.close();
+
 
     //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
     // SEARCH
@@ -95,48 +122,48 @@ main(int argc, char *argv[])
 
     // testData[] by now is populated
     // Let's populate search[] with random values to ensure that a value we search for is already present at an index
-    for (int i = 0; i < 100; i++)
-    {
-        i = rand();
-        search[i];
-    }
-    bool flag = false;
-    bool found = false; // for search loop
-    while (flag = false) // while not at the end of search[]
-    {
-        if (num_searched_items == 0)
-        {
-            time; // start the clock
-        }
-        // TODO: search through testData[] 100 items at a time
-        for (int i = 0; i < search[100]; i++)
-        {
-            if (testData[i] == search[i])
-            {
-                found = true; // it matches / found
-                cout << "testData at [ " << i << " ] is [ " << testData[i] << " ] which SHOULD equal to search[i] [ " << search[i] << endl;
-            }
-            else cout << "testData[ "<< testData[i] <<" ] != search[ "<< search[i] <<" ]" << endl;
-        }
+    // for (int i = 0; i < 100; i++)
+    // {
+    //     i = rand() % 100;
+    //     search[i];
+    // }
+    // bool flag = false;
+    // bool found = false; // for search loop
+    // while (flag = false) // while not at the end of search[]
+    // {
+    //     if (num_searched_items == 0)
+    //     {
+    //         time; // start the clock
+    //     }
+    //     // TODO: search through testData[] 100 items at a time
+    //     for (int i = 0; i < search[100]; i++)
+    //     {
+    //         if (testData[i] == search[i])
+    //         {
+    //             found = true; // it matches / found
+    //             cout << "testData at [ " << i << " ] is [ " << testData[i] << " ] which SHOULD equal to search[i] [ " << search[i] << endl;
+    //         }
+    //         else cout << "testData[ "<< testData[i] <<" ] != search[ "<< search[i] <<" ]" << endl;
+    //     }
 
-        if (num_searched_items == 99)
-        {
-            time = clock() - time;                   // stop the clock
-            duration = time / (float)CLOCKS_PER_SEC; // calculate how long it took to run the function.
+    //     if (num_searched_items == 99)
+    //     {
+    //         time = clock() - time;                   // stop the clock
+    //         duration = time / (float)CLOCKS_PER_SEC; // calculate how long it took to run the function.
 
-            float avg = duration / 100; // average insert time
-            search[search_index] = avg; // put the average in insert[]
+    //         float avg = duration / 100; // average insert time
+    //         search[search_index] = avg; // put the average in insert[]
 
-            // cout << "At SEARCH array index 100 * [ " << search_index << " ] the function took [ " << duration << " ] seconds to SEARCH \t";
-            // cout << "The AVERAGE SERACH TIME at SEARCH index 100 * [ " << search_index << " ] is [ " << search[search_index] << " ] seconds" << endl;
-            // cout << "----------------------------               o                --------------------------" << endl << endl;
+    //         // cout << "At SEARCH array index 100 * [ " << search_index << " ] the function took [ " << duration << " ] seconds to SEARCH \t";
+    //         // cout << "The AVERAGE SERACH TIME at SEARCH index 100 * [ " << search_index << " ] is [ " << search[search_index] << " ] seconds" << endl;
+    //         // cout << "----------------------------               o                --------------------------" << endl << endl;
 
-            // TODO: PRINT AVERAGES INTO 'array_insert_avg.csv' FILE
+    //         // TODO: PRINT AVERAGES INTO 'array_insert_avg.csv' FILE
 
-            search_index++;          // go to the next index in my search array for next time
-            num_searched_items = -1; // reset item counter so we can start the clock again
-        }
-        num_searched_items++;
-        if (search_index == 100) flag = true;
-    } // while() search
+    //         search_index++;          // go to the next index in my search array for next time
+    //         num_searched_items = -1; // reset item counter so we can start the clock again
+    //     }
+    //     num_searched_items++;
+    //     if (search_index == 100) flag = true;
+    // } // while() search
 }
