4afebbd98dab7235900a0c5092bd33e38ed5f1d1
Nicole Leon <nicole.leon.ny@gmail.com>
2020-07-23 20:17:48 -0600

    Doubly Linked List; HT Chaining and Quadratic probing; Began HT Chaining

---
+++ CSCI_2270_2020/homeworks/final_project/ht_linear_probing/hashlinear.hpp
@@ -0,0 +1,33 @@
+#ifndef HASHLINEAR_HPP
+#define HASHLINEAR_HPP
+
+#include <string>
+
+using namespace std;
+
+struct HashNode
+{
+    int key;
+    // struct HashNode *next;
+};
+
+class HashTable
+{
+public:
+    HashTable(int tableSize);               // Constructor
+    ~HashTable() { delete[] table; }
+    bool insertItem(int key);               // inserts a key into hash table
+    unsigned int hashFunction(int key);  // hash function to map values to key
+    void printTable();
+    int getNumOfCollision();
+    HashNode *searchItem(int key);
+
+private:
+    int tableSize; // No. of buckets (linked lists)
+
+    HashNode **table; // Pointer to an array containing buckets
+    int numOfcolision = 0;
+    HashNode *createhashNode(int key, HashNode *next);
+};
+
+#endif
