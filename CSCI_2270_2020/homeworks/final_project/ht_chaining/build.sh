b8916a45ed26282df277da379c41702549d3074e
gubatron <gubatron@gmail.com>
2020-07-23 21:57:35 -0600

    remove chaining executable before building

--- CSCI_2270_2020/homeworks/final_project/ht_chaining/build.sh
+++ CSCI_2270_2020/homeworks/final_project/ht_chaining/build.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+if [ -f ht_chaining.exe ]; then
+    rm ht_chaining.exe
+fi
 echo "Building ht_chaining..."
 g++ -std=c++11 hashchain.cpp hashchaindriver.cpp -o ht_chaining.exe
 if [ $? -ne 0 ]; then
